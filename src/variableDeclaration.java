2c92ec0b53a55e5135acfe6a35043c43eae3045e
Navadeepan <deepannava114@gmail.com>
2024-02-11 12:37:19 +0530

    change in variableDeclaration.java

--- src/variableDeclaration.java
+++ src/variableDeclaration.java
@@ -2,7 +2,7 @@ public class variableDeclaration {
     //local variable declaration
     public void ageverify(){
         int age = 18; //Local variable
-        System.out.print("Age is above 18");
+        System.out.print("Age is above 18 ");
     }
 
     public static void main(String[] args){
