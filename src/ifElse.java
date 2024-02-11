d34d96a23276460524836b21ac8d37350abdf787
Navadeepan <deepannava114@gmail.com>
2024-02-11 21:38:40 +0530

    Day 1 conditional operations

---
+++ src/ifElse.java
@@ -0,0 +1,14 @@
+public class ifElse {
+    public static void main(String[] args){
+        int age = 18;
+        if(age>18){
+            System.out.println("age is above 18");
+        }
+        else if (age==18) {
+            System.out.println("Age is equal to 18");
+        }
+        else{
+            System.out.println("age is below 18");
+        }
+    }
+}
